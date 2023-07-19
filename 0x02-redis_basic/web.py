a24babdf2b9bac97b0866f7ea93b6d13ffe137b9
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2023-07-19 17:07:14 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,47 +3,43 @@
 web cache and tracker
 """
 import requests
+import redis
 import time
-from functools import lru_cache
 
-# Dictionary to track URL access count
-url_access_count = {}
-
+# Connect to Redis
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
 def get_page(url: str) -> str:
-    # Increment URL access count
-    url_access_count[f"count:{url}"] = url_access_count.get(f"count:{url}", 0) + 1
+    # Check if the page content is already cached in Redis
+    cached_html_content = redis_client.get(url)
+    if cached_html_content is not None:
+        # Increment URL access count
+        redis_client.incr(f"count:{url}")
+        return cached_html_content
 
-    # Use requests to fetch the HTML content
+    # If not cached, fetch the HTML content using requests
     response = requests.get(url)
     html_content = response.text
 
+    # Cache the HTML content in Redis with an expiration time of 10 seconds
+    redis_client.setex(url, 10, html_content)
+
+    # Increment URL access count
+    redis_client.incr(f"count:{url}")
+
     return html_content
-
-
-# Use lru_cache decorator to cache the result with an expiration time of 10 seconds
-@lru_cache(maxsize=None, typed=True)
-def cached_get_page(url: str) -> str:
-    return get_page(url)
-
 
 # Test the get_page function with caching and URL access count tracking
 if __name__ == "__main__":
     url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.example.com"
 
-    # Access the URL multiple times to test caching
+    # Access the URL multiple times to test caching and tracking
     for _ in range(5):
         start_time = time.time()
-        content = cached_get_page(url)
-        print(f"URL content fetched in {time.time() - start_time} seconds")
+        content = get_page(url)
+        print(f"URL content fetched in {time.time() - start_time:.2f} seconds")
 
     # Print the URL access count
-    print(url_access_count)
+    url_access_count = redis_client.get(f"count:{url}")
+    print(f"{url} accessed {url_access_count} times")
 
-
-
-@count_url_access
-def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
