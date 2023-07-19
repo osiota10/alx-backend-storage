6516bd94ee239cfb65e795389efbe9598c41e281
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2023-07-19 17:12:08 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,45 +1,21 @@
-import requests
+#!/usr/bin/env python3
+"""
+create a web cach
+"""
 import redis
-from functools import wraps
-
-# Connect to Redis
-store = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
-
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.setex(cached_key, 10, html)
-        return html
-    return wrapper
+import requests
+rc = redis.Redis()
+count = 0
 
 
-@count_url_access
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
-# Test the get_page function with caching and URL access count tracking
+
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.example.com"
-
-    # Access the URL multiple times to test caching and tracking
-    for _ in range(5):
-        content = get_page(url)
-        print("URL content fetched")
-
-    # Print the URL access count
-    count_key = "count:" + url
-    url_access_count = store.get(count_key)
-    print(f"{url} accessed {url_access_count} times")
+    get_page('http://slowwly.robertomurray.co.uk')
